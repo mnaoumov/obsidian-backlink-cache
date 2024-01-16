8d360c6d80c5a6438a75776206b0123fd502a650
Michael Naumov <mnaoumov@gmail.com>
2024-01-16 16:12:13 -0600

    Migrate to new template

--- src/OriginalFunc.ts
+++ src/OriginalFunc.ts
@@ -1,9 +1,9 @@
 type OriginalFuncWrapper<T> = { originalFunc?: T };
 
 export function getOriginalFunc<T>(func: T): T | undefined {
-    return (func as OriginalFuncWrapper<T>).originalFunc;
+  return (func as OriginalFuncWrapper<T>).originalFunc;
 }
 
 export function setOriginalFunc<T>(func: T, originalFunc: T): void {
-    (func as OriginalFuncWrapper<T>).originalFunc = originalFunc;
+  (func as OriginalFuncWrapper<T>).originalFunc = originalFunc;
 }
