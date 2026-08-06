12a7a025f16cc126b52fd72535b8c8ea0ffa6332
Michael Naumov <mnaoumov@gmail.com>
2026-08-05 20:30:32 -0600

    chore: update libs
    Bump obsidian-dev-utils to 90.0.1 and obsidian-test-mocks to 3.11.0.
    
    ODU 90 makes CommandHandlerComponent.registerCommandHandlers async, so
    onloadImpl awaits it and is declared async.

--- src/plugin.ts
+++ src/plugin.ts
@@ -10,7 +10,7 @@ import { PluginSettingsComponent } from './plugin-settings-component.ts';
 import { PluginSettingsTab } from './plugin-settings-tab.ts';
 
 export class Plugin extends PluginBase {
-  protected override onloadImpl(): void {
+  protected override async onloadImpl(): Promise<void> {
     const pluginSettingsComponent = this.addChild(
       new PluginSettingsComponent({
         dataHandler: new PluginDataHandler(this),
@@ -39,7 +39,7 @@ export class Plugin extends PluginBase {
       })
     );
 
-    this.commandHandlerComponent.registerCommandHandlers(() => [
+    await this.commandHandlerComponent.registerCommandHandlers(() => [
       new RefreshBacklinkPanelsCommandHandler(backlinkCacheComponent),
       new OpenDemoVaultCommandHandler({
         app: this.app,
